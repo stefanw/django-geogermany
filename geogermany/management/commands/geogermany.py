20f5a4b5ac8a19df102ba7c460c00609a2d5a93b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2016-05-10 15:14:57 +0200

    Make parser nicer

--- geogermany/management/commands/geogermany.py
+++ geogermany/management/commands/geogermany.py
@@ -1,34 +1,33 @@
 import os
-from datetime import datetime
-
-import pytz
 
 from django.core.management.base import BaseCommand
 from django.contrib.gis.gdal import DataSource
 from django.contrib.gis.utils import LayerMapping
+from django.contrib.gis.geos.error import GEOSException
 
 from slugify import slugify
 
-from ...models import State, District, Municipality, ZipCode
+from ...models import GermanGeoArea, State, District, Municipality, ZipCode
 
 
 class Command(BaseCommand):
     help = "geogermany"
 
-    def handle(self, *args, **options):
-        if hasattr(self, args[0]):
-            getattr(self, args[0])(*args[1:], **options)
-        else:
-            print "Sub-Command not found"
+    def add_arguments(self, parser):
+        parser.add_argument('command', help='Subcommand')
+        parser.add_argument('base_path', help='Add base path')
 
-    def load(self, *args, **options):
-        base_path = args[0]
+    def handle(self, *args, **options):
+        getattr(self, options['command'])(**options)
+
+    def load(self, **options):
+        base_path = options['base_path']
         self.stdout.write("\nState\n")
-        self.load_by_path(State, base_path, 'VG250_LAN.shp')
+        self.load_by_path(State, base_path, 'bundeslaender.shp')
         self.stdout.write("\nDistrict\n")
-        self.load_by_path(District, base_path, 'VG250_KRS.shp')
+        self.load_by_path(District, base_path, 'landkreise.shp')
         self.stdout.write("\nMunicipality\n")
-        self.load_by_path(Municipality, base_path, 'VG250_GEM.shp')
+        self.load_by_path(Municipality, base_path, 'gemeinden.shp')
         self.stdout.write("\nZipCode\n")
         self.load_zip(ZipCode, base_path, 'plz.geojson')
 
@@ -42,27 +41,29 @@ class Command(BaseCommand):
         layer = ds[0]
         count = float(len(layer))
         for i, feature in enumerate(layer):
-            if feature['GF'].as_int() != 4:
-                continue
             self.stdout.write('%.2f%%\r' % (i / count * 100), ending='')
             name = feature['GEN'].as_string()
-            slug = slugify(name)
+            kind_detail = feature['DES'].as_string()
+            slug = slugify(u'%s %s' % (kind_detail, name))
 
             geom = mapping.feature_kwargs(feature)['geom']
 
-            validity = feature['WSK'].as_string()
-            if validity:
-                naive = datetime.strptime(validity, '%Y/%m/%d')
-                validity = pytz.timezone("Europe/Berlin").localize(naive, is_dst=None)
-            else:
-                validity = None
-            klass.objects.get_or_create(slug=slug, defaults={
+            # validity = feature['WSK'].as_string()
+            # if validity:
+            #     naive = datetime.strptime(validity, '%Y/%m/%d')
+            #     validity = pytz.timezone("Europe/Berlin").localize(naive, is_dst=None)
+            # else:
+            validity = None
+            kind = klass.__name__.lower()
+
+            klass.objects.get_or_create(slug=slug, kind=kind, defaults={
                 'name': name,
                 'rs': feature['RS'].as_string(),
-                'ags': feature['AGS'].as_string(),
-                'kind': feature['BEZ'].as_string(),
-                'nuts': feature['NUTS'].as_string(),
-                'population': feature['EWZ'].as_int(),
+                'ags': feature['AGS'].as_string() if 'AGS' in feature else '',
+                'kind': kind,
+                'kind_detail': kind_detail,
+                'nuts': feature['NUTS'].as_string() if 'NUTS' in feature else '',
+                'population': (feature['EWZ_W'].as_int() + feature['EWZ_M'].as_int()) if 'EWZ_W' in feature else None,
                 'geom': geom,
                 'area': feature.geom.area,
                 'valid_on': validity
@@ -80,43 +81,36 @@ class Command(BaseCommand):
             klass.objects.get_or_create(slug=slug, defaults={
                 'name': name,
                 'geom': geom,
-                'area': feature.geom.area
+                'area': feature.geom.area,
+                'kind': 'zipcode'
             })
 
     def create_hierarchy(self, *args, **options):
-        for district in District.objects.all():
-            state = State.objects.filter(geom__covers=district.geom.centroid)
-            if len(state) == 1:
-                district.part_of = state[0]
-                district.save()
-            elif len(state) == 0:
-                print district, "0 states"
-            else:
-                print district, state
-
-        for muni in Municipality.objects.all():
-            dis = District.objects.filter(geom__covers=muni.geom.centroid)
-            if len(dis) == 1:
-                muni.part_of = dis[0]
-                muni.save()
-            elif len(dis) == 0:
-                print muni, "0 districts"
-            else:
-                print muni, dis
-
-        for zipcode in ZipCode.objects.all():
-            print zipcode
-            state = State.objects.filter(geom__covers=zipcode.geom.centroid)
-            if len(state) == 1:
-                zipcode.part_of = state[0]
-                zipcode.save()
-            elif len(state) == 0:
-                print zipcode, "0 states"
-            else:
-                print zipcode, state
+        matches = [
+            ('district', 'state'),
+            ('municipality', 'district'),
+            ('zipcode', 'state')
+        ]
+        for small, big in matches:
+            for small_obj in GermanGeoArea.objects.filter(kind=small,
+                                                          part_of__isnull=True):
+                print('Trying', small_obj)
+                try:
+                    big_objs = GermanGeoArea.objects.filter(kind=big, geom__covers=small_obj.geom.point_on_surface)
+                except GEOSException:
+                    big_objs = GermanGeoArea.objects.filter(kind=big, geom__covers=small_obj.geom.centroid)
+                if not big_objs:
+                    big_objs = GermanGeoArea.objects.filter(kind=big, geom__intersects=small_obj.geom)
+                if len(big_objs) == 1:
+                    small_obj.part_of = big_objs[0]
+                    small_obj.save()
+                    print(small_obj, 'assigned', big_objs[0])
+                elif len(big_objs) == 0:
+                    print(small_obj, "0", big)
+                else:
+                    print(small_obj, 'too many', big_objs)
 
     def calculate_area(self, *args, **options):
-        for klass in (State, Municipality, District, ZipCode):
-            for obj in klass.objects.all():
-                obj.calculate_area()
-                obj.save()
+        for obj in GermanGeoArea.objects.all():
+            obj.calculate_area()
+            obj.save()
